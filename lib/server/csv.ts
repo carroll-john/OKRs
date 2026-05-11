33e17f1b8d22c209b750df46470988e977c5a37f
John Carroll <jc@Johns-MacBook-Air.local>
2026-05-11 22:41:31 +1000

    chore: reset project

--- lib/server/csv.ts
+++
@@ -1,8 +0,0 @@
-export function csvEscape(value: string | number): string {
-  const str = String(value ?? '');
-  if (str.includes(',') || str.includes('"') || str.includes('\n')) {
-    return `"${str.replace(/"/g, '""')}"`;
-  }
-
-  return str;
-}
